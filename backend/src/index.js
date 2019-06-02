9d49a5286c852c6e5c2b99bc1bdb408d70caaaff
MichaelE1 <mickel13@gmail.com>
2019-06-02 13:54:49 +1000

    set up graphql yoga

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,21 @@
-// let's go!
+require("dotenv").config({ path: "variables.env" });
+const CreateServer = require("./createServer");
+const db = require("./db");
+
+const server = CreateServer();
+
+// TO DO: use express middleware to handle cookies
+
+// TO DO: use express middleware to populate current user
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    }
+  },
+  deets => {
+    console.log(`Server is now running on port http://localhost:${deets.port}`);
+  }
+);
